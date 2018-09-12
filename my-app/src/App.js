acb2267fde05e6b827c5d4d658c8f306a6a7b648
milelym <milelymarcial@gmail.com>
2018-09-12 12:39:12 -0300

    se agregan las vistas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './componentes/Login'
+import Login from './componentes/Login';
+import Register from './componentes/Register';
+import Wall from './componentes/Wall';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+    <div className="App">
         <Login />
-        <Register />
+        /*<Register />*/
         <Wall />
       </div>
     );
