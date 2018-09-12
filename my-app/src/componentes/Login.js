4f97f92e530ffde6fc136034ba9ac0680f29d248
Natalia <nataliamh.88@gmail.com>
2018-09-12 12:46:53 -0300

    probando el logIn

--- my-app/src/componentes/Login.js
+++ my-app/src/componentes/Login.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import Img from './componentesLogin/Img';
 import ImagenLogo from '../assets/img/logo.png'
+=======
+import Navbar from './componentesWall/Navbar';
+import Img from './componentesLogin/Img';
+>>>>>>> probando el logIn
 
 class App extends Component {
   render() {
     return (
       <div>
+<<<<<<< HEAD
         <Img photo= {ImagenLogo} />
+=======
+        <Navbar />
+        <Img photo="../assets/img/logo.png" />
+>>>>>>> probando el logIn
       </div>
     );
   };
