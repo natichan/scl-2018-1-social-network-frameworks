3de9d294d8421f43a113c76d31fb49abc15433f8
Natalia <nataliamh.88@gmail.com>
2018-09-12 12:50:10 -0300

    enlace imagen

--- my-app/src/componentes/Login.js
+++ my-app/src/componentes/Login.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Img from './componentesLogin/Img';
 import ImagenLogo from '../assets/img/logo.png'
 =======
 import Navbar from './componentesWall/Navbar';
 import Img from './componentesLogin/Img';
 >>>>>>> probando el logIn
+=======
+import Img from './componentesLogin/Img';
+import ImagenLogo from '../assets/img/logo.png'
+>>>>>>> enlace imagen
 
 class App extends Component {
   render() {
     return (
       <div>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <Img photo= {ImagenLogo} />
 =======
         <Navbar />
         <Img photo="../assets/img/logo.png" />
 >>>>>>> probando el logIn
+=======
+        <Img photo= {ImagenLogo} />
+>>>>>>> enlace imagen
       </div>
     );
   };
