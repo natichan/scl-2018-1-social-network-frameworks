39832ed352ab090b4a000ccbe487431771d4ddd5
milelym <milelymarcial@gmail.com>
2018-09-12 12:41:13 -0300

    enlace del wall  y navbar

---
+++ my-app/src/componentes/Wall.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from './componentesWall/Navbar'
+
+class Wall extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Navbar />
+        </div> 
+      );
+    }
+  }
+
+export default Wall;
