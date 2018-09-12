5e216d88b26d8d90f25ca58102f1dc9b0b39038d
milelym <milelymarcial@gmail.com>
2018-09-12 12:43:17 -0300

    enlace register  a registration

---
+++ my-app/src/componentes/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Registration from './componentesRegister/Registration'
+
+class Register extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Registration />
+        </div>
+      );
+    }
+  }
+
+export default Register;
