daf6f10a94281ee877632a6190db836501c9fadb
Natalia <nataliamh.88@gmail.com>
2018-09-14 13:47:01 -0300

    formulario iniciar sesion

--- my-app/src/componentes/Login.js
+++ my-app/src/componentes/Login.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import Img from './componentesLogin/Img';
 import ImagenLogo from '../assets/img/logo.png'
 import Button from './componentesLogin/ButtonFG'
+import Init from './componentesLogin/Init'
+
 
 class Login extends Component {
   render() {
     return (
       <div>
         <Img photo= {ImagenLogo}/>
+        <Init />
         <Button />
       </div>
     );
