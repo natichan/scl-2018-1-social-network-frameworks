80ee0a185afb0c25ff987a35f98790743a4cce33
milelym <milelymarcial@gmail.com>
2018-09-14 13:20:50 -0300

    se agrega boton

--- my-app/src/componentes/componentesRegister/Registration.js
+++ my-app/src/componentes/componentesRegister/Registration.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Register.css';
-import {Row, Input, Icon} from 'react-materialize'
+import {Row, Input, Icon, Button} from 'react-materialize'
 import {Col} from 'react-materialize'
 
 class InputAdornments extends React.Component {
@@ -13,6 +13,9 @@ class InputAdornments extends React.Component {
     <Col s={12}>
       <Input s={12} type="email" label="Email"><Icon>account_circle</Icon></Input>
       <Input s={12} type="password" label="password" ><Icon>lock</Icon></Input>
+      <Row  className='ButtonLarge'>
+      <Button className='ButtonLarge1' waves='light' node='a' href='http://www.google.com'> REGISTRATE </Button>
+      </Row>
     </Col>
     </Row>
     );
