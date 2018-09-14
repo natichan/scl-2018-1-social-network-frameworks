de8c7fbacd9ce522a3ece25c7b6e92d2c3f4ca3c
milelym <milelymarcial@gmail.com>
2018-09-14 12:23:05 -0300

    iconos

--- my-app/src/componentes/componentesRegister/Registration.js
+++ my-app/src/componentes/componentesRegister/Registration.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Register.css';
-import {Row, Input} from 'react-materialize'
+import {Row, Input, Icon} from 'react-materialize'
 import {Col} from 'react-materialize'
-import CardPanel from 'react-materialize'
 
 class InputAdornments extends React.Component {
 
@@ -12,8 +11,8 @@ class InputAdornments extends React.Component {
 
     <Row className="example">
     <Col s={12}>
-        <Input s={12} type="password" label="password" />
-        <Input s={12} type="email" label="Email" />
+      <Input s={12} type="email" label="Email"><Icon>account_circle</Icon></Input>
+      <Input s={12} type="password" label="password" ><Icon>lock</Icon></Input>
     </Col>
     </Row>
     );
