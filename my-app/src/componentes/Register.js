a1bc6c89ad5b98862790a8df7742ef1c12fee60d
milelym <milelymarcial@gmail.com>
2018-09-14 11:41:12 -0300

    se agregan input y logo

--- my-app/src/componentes/Register.js
+++ my-app/src/componentes/Register.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Registration from './componentesRegister/Registration'
+import Img from './../componentes/componentesLogin/Img'
+import ImagenLogo from './../assets/img/logo.png'
+
 
 class Register extends Component {
     render() {
       return (
         <div className="App">
+          <Img photo= {ImagenLogo}/>
           <Registration />
         </div>
       );
