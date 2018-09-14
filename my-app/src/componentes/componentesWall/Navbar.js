49f9a809eec91bde2f76335f356be09ceb6f997e
Natalia <nataliamh.88@gmail.com>
2018-09-14 14:44:56 -0300

    creando navbar

--- my-app/src/componentes/componentesWall/Navbar.js
+++ my-app/src/componentes/componentesWall/Navbar.js
@@ -1,3 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Navbar, NavItem, Icon} from 'react-materialize';
+import Img from '../componentesLogin/Img';
 
 
+class NavbarC extends Component {
+    render(){
+        return (
+            <Navbar brand='logo' right>
+                <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+                <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+                <Img />
+            </Navbar>
+        )
+    }
+}
+
+export default NavbarC;
