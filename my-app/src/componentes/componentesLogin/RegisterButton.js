9f4624edaac6c53e6bc2e40ecbb32686b1eac8b5
Natalia <nataliamh.88@gmail.com>
2018-09-14 14:44:45 -0300

    register button

---
+++ my-app/src/componentes/componentesLogin/RegisterButton.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+// import {Button} from 'react-materialize';
+import './Login.css'
+
+
+class RegisterButton extends Component {
+    render() {
+        return (
+         <div className='body'>
+            <a waves='light' className= 'register' node='a' href=''> Regístrate </a>
+         </div>
+        )
+    }
+}
+
+export default RegisterButton;
