ddb6312f033bd6effda90d549f26297d898b578b
milelym <milelymarcial@gmail.com>
2018-09-14 10:47:22 -0300

    input

--- my-app/src/componentes/componentesRegister/Registration.js
+++ my-app/src/componentes/componentesRegister/Registration.js
@@ -1,42 +1,24 @@
 import React from 'react';
 import './Register.css';
-import TextField from '@material-ui/core/TextField';
-import AccountCircle from '@material-ui/icons/AccountCircle';// primera letra mayuscula 
-import PropTypes from 'prop-types';
-import InputAdornment from '@material-ui/core/InputAdornment';
-
-
-
-
-// const ranges = [
-//   {
-//     value: '0-20',
-//     label: '0 to 20',
-//   },
-//   {
-//     value: '21-50',
-//     label: '21 to 50',
-//   },
-//   {
-//     value: '51-100',
-//     label: '51 to 100',
-//   },
-// ];
+import {Row, Input} from 'react-materialize'
+import {Col} from 'react-materialize'
 
 class InputAdornments extends React.Component {
 
   render() {
 
     return (
-<Row>
-    <Input placeholder="Placeholder" s={6} label="First Name" />
-    <Input s={6} label="Last Name" />
-    <Input s={12} label="disabled" defaultValue="I am not editable" disabled />
-    <Input type="password" label="password" s={12} />
-    <Input type="email" label="Email" s={12} />
-</Row>
-     
+    <Row >
+    <Col s={12} className='grid-example'>
+      <Input type="email" label="Email" s={6} />
+    </Col>
+    <Col s={12} className='grid-example'>
+      <Input type="password" label="password" s={6} />
+    </Col>
+
+    </Row>
     );
+  
   }
 }
 
