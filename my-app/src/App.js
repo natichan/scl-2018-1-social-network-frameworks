8fd2a24f3985e1c29898df3de4003ba6c567fbd9
Natalia <nataliamh.88@gmail.com>
2018-09-16 20:51:40 -0300

    probando muro navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Login from './componentes/Login';
-//import Register from './componentes/Register';
+import Login from './componentes/Login';
+// import Register from './componentes/Register';
 import Wall from './componentes/Wall';
 
 class App extends Component {
   render() {
     return (
     <div>
+        {/* <Register /> */}
+        {/* <Login /> */}
         <Wall />
     </div>
     );
