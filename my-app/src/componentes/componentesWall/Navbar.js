af4e57d6021b9ecd9ac74714472176f9233e3c76
Natalia <nataliamh.88@gmail.com>
2018-09-16 20:51:23 -0300

    navbar

--- my-app/src/componentes/componentesWall/Navbar.js
+++ my-app/src/componentes/componentesWall/Navbar.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import {Navbar, NavItem, Icon} from 'react-materialize';
+import {Navbar,NavItem, Icon} from 'react-materialize';
 import LogoNavbar from './LogoNavbar';
-import ImagenLogo from '../../assets/img/logotipoHorizontalBordeBlanco (1).png'
-
+import ImageLogo from '../../assets/img/logotipoHorizontalBordeBlanco (1).png'
+import './wall.css'
 
 class NavbarC extends Component {
     render(){
         return (
-            <div className="navbar" >
-                <a href='' className="logo"><Icon>reorder</Icon></a>
-                {/* <Img photo= {ImagenLogo}/> */}
-                <LogoNavbar photo={ImagenLogo} />
-            </div>
+            <Navbar brand={<LogoNavbar photo = {ImageLogo} />} className="navbar" right>
+            <NavItem onClick={() => console.log('test click')} ><Icon>home</Icon><p className="textIcon">Wall</p></NavItem>
+            <NavItem onClick={() => console.log('test click')} ><Icon>person</Icon><p className="textIcon">Profile</p></NavItem>
+            <NavItem onClick={() => console.log('test click')}  ><Icon>person_add</Icon><p className="textIcon">Friends</p></NavItem>
+            <NavItem onClick={() => console.log('test click')}  ><Icon>keyboard_tab</Icon><p className="textIcon">Log out</p></NavItem>
+            </Navbar>
         )
     }
 }
