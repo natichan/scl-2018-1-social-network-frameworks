543d9404fc5c707edf75b1b29107d2419aa0fa17
milelym <milelymarcial@gmail.com>
2018-09-16 22:27:42 -0300

    prueba wall

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 //import Login from './componentes/Login';
-import Register from './componentes/Register';
-//import Wall from './componentes/Wall';
+//import Register from './componentes/Register';
+import Wall from './componentes/Wall';
 
 class App extends Component {
   render() {
     return (
     <div>
-        <Register />
+        <Wall />
     </div>
     );
   }
