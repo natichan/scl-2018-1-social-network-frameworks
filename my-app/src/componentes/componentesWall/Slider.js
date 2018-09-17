6e37705a7965b2d8defe29bf85be13a49fe9878b
milelym <milelymarcial@gmail.com>
2018-09-17 01:08:44 -0300

    pruebas muro input y boton

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import ImagenLogo from './../../assets/img/veganHamburguer.jpg';
-import {Row, Slider, Slide, Col, Input} from 'react-materialize'
+import {Row, Slider, Slide, Col, Input, Button} from 'react-materialize'
 import imgh from './../../assets/img/veganHamburguer.jpg'
 import imgI from './../../assets/img/vegan-food-selection-t.jpg'
 import imgJ from './../../assets/img/green-kitchen-stories-meal-inspiration.jpg'
@@ -8,22 +8,20 @@ import './wall.css'
 class App extends Component {
     render() {
       return (
-        <Row className='inputxt'>
-          <Col s={12}  >
+        <Row className=' white inputxt'>
+          <Col  s={12} className=' imgu' >
           <Slider >
-            <Slide 
-              src={imgh} >
+            <Slide className='imgu' src={imgh} >
             </Slide>
-            <Slide
-              src={imgI}>
+            <Slide src={imgI}>
             </Slide>
-            <Slide
-              src={imgJ}>
+            <Slide src={imgJ}>
             </Slide>
           </Slider> 
           </Col>
           <div >
-          <Input type='text' s={12}  />
+          <Input type='text' s={10}  />
+          <Button waves='light' className='green'>Publicar</Button>
 
           </div>
         </Row> 
