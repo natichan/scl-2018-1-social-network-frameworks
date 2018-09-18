97bbb9b07c71ec3d731fe857ab47b6228271e970
Natalia <nataliamh.88@gmail.com>
2018-09-18 18:11:12 -0300

    form de perfil con cambio de input

--- my-app/src/componentes/componentsProfile/Form.js
+++ my-app/src/componentes/componentsProfile/Form.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
-
+import {Row, Input} from 'react-materialize';
+import Navbar from '../componentesWall/Navbar'
 
 class Form extends Component {
     render() {
         return (
-            <Row>
-                <Input placeholder="Placeholder" s={6} label="First Name" />
-                <Input s={6} label="Last Name" />
-                <Input label="Birthday" s={12} />
-                <Input type="email" label="Email" s={12} />
-            </Row>
+            <div>
+            <Navbar />
+                <Row>
+                    <Input placeholder="Placeholder" s={6} label="First Name" />
+                    <Input s={6} label="Last Name" />
+                    <Input label="Birthday" s={12} />
+                    <Input label="About me" s={12} />
+                </Row>
+            </div>
         )
     }
 }
