bd93cfc715d66523bc4706042229918d379e23a8
Natalia <nataliamh.88@gmail.com>
2018-09-18 18:10:37 -0300

    form incorporada a componente perfil

---
+++ my-app/src/componentes/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Form from './componentsProfile/Form'
+
+class Profile extends Component {
+    render() {
+      return (
+        <div>
+          <Form />
+        </div> 
+      );
+    }
+  }
+
+export default Profile;
