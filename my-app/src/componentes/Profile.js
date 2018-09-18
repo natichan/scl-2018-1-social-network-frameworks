555b7f64149aac74225a7880b70e36c8c8ce1017
Natalia <nataliamh.88@gmail.com>
2018-09-18 18:15:53 -0300

    incorporando imagen

--- my-app/src/componentes/Profile.js
+++ my-app/src/componentes/Profile.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Form from './componentsProfile/Form'
+import Form from './componentsProfile/Form';
+import Picture from './componentsProfile/Picture';
+import ExamplePic from '../assets/img/foto-perfil.jpg'
 
 class Profile extends Component {
     render() {
       return (
         <div>
-          <Form />
+            <Picture photo = {ExamplePic}/>
+            <Form />
         </div> 
       );
     }
