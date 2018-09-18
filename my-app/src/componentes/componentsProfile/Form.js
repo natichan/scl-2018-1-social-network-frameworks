3f79d2293dc07c4b20722fa9a1d40a4ad46ee60d
Natalia <nataliamh.88@gmail.com>
2018-09-18 17:59:10 -0300

    creando form de perfil

---
+++ my-app/src/componentes/componentsProfile/Form.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Form extends Component {
+    render() {
+        return (
+            <Row>
+                <Input placeholder="Placeholder" s={6} label="First Name" />
+                <Input s={6} label="Last Name" />
+                <Input label="Birthday" s={12} />
+                <Input type="email" label="Email" s={12} />
+            </Row>
+        )
+    }
+}
