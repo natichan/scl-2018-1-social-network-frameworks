75e2b5bf6ef2d0300ccdb978d798366a3827e6de
Natalia <nataliamh.88@gmail.com>
2018-09-18 18:18:18 -0300

    saque navbar

--- my-app/src/componentes/componentsProfile/Form.js
+++ my-app/src/componentes/componentsProfile/Form.js
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
 import {Row, Input} from 'react-materialize';
-import Navbar from '../componentesWall/Navbar'
 
 class Form extends Component {
     render() {
         return (
-            <div>
-            <Navbar />
-                <Row>
-                    <Input placeholder="Placeholder" s={6} label="First Name" />
-                    <Input s={6} label="Last Name" />
-                    <Input label="Birthday" s={12} />
-                    <Input label="About me" s={12} />
-                </Row>
-            </div>
+            <Row>
+                <Input placeholder="Placeholder" s={6} label="First Name" />
+                <Input s={6} label="Last Name" />
+                <Input label="Birthday" s={12} />
+                <Input label="About me" s={12} />
+            </Row>
         )
     }
 }
