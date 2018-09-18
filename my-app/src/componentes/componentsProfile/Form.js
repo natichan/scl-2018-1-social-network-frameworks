a64d0699bd3081edb4b25e390be5c63da0aeb95a
Natalia <nataliamh.88@gmail.com>
2018-09-18 18:39:13 -0300

    textarea para escribir sobre la persona

--- my-app/src/componentes/componentsProfile/Form.js
+++ my-app/src/componentes/componentsProfile/Form.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import './profile.css';
 import {Row, Input} from 'react-materialize';
 
 class Form extends Component {
     render() {
         return (
-            <Row>
+            <Row>           
                 <Input placeholder="Placeholder" s={6} label="First Name" />
                 <Input s={6} label="Last Name" />
                 <Input label="Birthday" s={12} />
-                <Input label="About me" s={12} />
+                <Input type="textarea" label="About me" s={12}/>             
             </Row>
         )
     }
