92df38a2899549f0055f50f2f348d55a42232a87
Natalia <nataliamh.88@gmail.com>
2018-09-19 19:33:56 -0300

    ruta de navbar

--- my-app/src/componentes/componentesWall/Navbar.js
+++ my-app/src/componentes/componentesWall/Navbar.js
@@ -1,21 +1,54 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Navbar,NavItem, Icon} from 'react-materialize';
 import LogoNavbar from './LogoNavbar';
 import ImageLogo from '../../assets/img/logotipoHorizontalBordeBlanco (1).png'
 import './wall.css'
+import { Link } from 'react-router-dom';
+import * as routes from '../../constants/routes';
 
-class NavbarC extends Component {
-    render(){
-        return (
-            <Navbar brand={<LogoNavbar photo = {ImageLogo} />} className="navbar" right>
-            <NavItem onClick={() => console.log("test click")} ><Icon>home</Icon><p className="textIcon">Wall</p></NavItem>
-            <NavItem onClick={() => console.log("test click")} ><Icon>person</Icon><p className="textIcon">Profile</p></NavItem>
-            <NavItem onClick={() => console.log("test click")} ><Icon>person_add</Icon><p className="textIcon">Friends</p></NavItem>
-            <NavItem divider />
-            <NavItem onClick={() => console.log("test click")} ><Icon>keyboard_tab</Icon><p className="textIcon">Log out</p></NavItem>
-            </Navbar>
-        )
-    }
-}
+const NavbarComponent = () => {
+    return (
+    <Navbar brand={<LogoNavbar photo = {ImageLogo} />} className="navbar" right>   
+        <NavItem>
 
-export default NavbarC;
+        <Link to={routes.WALL}>
+            <Icon>home</Icon>
+            <p className="textIcon">Wall</p>
+        </Link>
+        </NavItem>
+
+
+           <NavItem>
+
+        <Link to={routes.PROFILE}>
+            <Icon>person</Icon>
+            <p className="textIcon">Profile</p>
+        </Link>
+        </NavItem>
+
+        <NavItem>
+
+        <Link to={routes.FRIENDS}>
+
+            
+            <p className="textIcon">Friends</p>
+            <Icon>person_add</Icon>
+        </Link>
+        </NavItem>
+
+
+    <NavItem divider />
+    <NavItem>
+
+        <Link to={routes.LOGIN}>
+
+            <Icon>keyboard_tab</Icon>
+            <p className="textIcon">Log out</p>
+        </Link>    
+        </NavItem>
+
+    </Navbar>
+    )
+}  
+
+export default NavbarComponent;
