83cb218a7cb0213296c38f17af227690a4d4cbc5
milelym <milelymarcial@gmail.com>
2018-09-19 00:28:05 -0300

    cambios slidr input boton

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -4,11 +4,14 @@ import {Row, Slider, Slide, Col, Input, Button} from 'react-materialize'
 import imgh from './../../assets/img/veganHamburguer.jpg'
 import imgI from './../../assets/img/vegan-food-selection-t.jpg'
 import imgJ from './../../assets/img/green-kitchen-stories-meal-inspiration.jpg'
+import LogoNavbar from './LogoNavbar';
+import ImageLogo from '../../assets/img/logotipoHorizontalBordeBlanco (1).png'
+
 import './wall.css'
 class App extends Component {
     render() {
       return (
-        <Row className=' white inputxt'>
+        <Row className=' white inputxt '>
           <Col  s={12} className=' imgu' >
           <Slider >
             <Slide className='imgu' src={imgh} >
@@ -19,11 +22,11 @@ class App extends Component {
             </Slide>
           </Slider> 
           </Col>
-          <div >
-          <Input type='text' s={10}  />
-          <Button waves='light' className='green'>Publicar</Button>
+          <Col l={12}>
+          <Input type='text'placeholder='¿Que tal tu día Veggie?'  s={12} />
+          <Button waves='light' className='green' s={12}>Publicar</Button>
 
-          </div>
+          </Col>
         </Row> 
       );
     }
