96090271d0cc3386d8179550d5c5d3b252d4ba96
milelym <milelymarcial@gmail.com>
2018-09-19 00:29:19 -0300

    visializar wall

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //import Login from './componentes/Login';
 // import Register from './componentes/Register';
 import Wall from './componentes/Wall';
+//import Post from './componentes/componentesWall/Post';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
     <div>
         {/* <Register /> */}
         {/* <Login /> */}
-        <Wall />
+        <Wall /> 
+        {/* <Post/> */}
     </div>
     );
   }
