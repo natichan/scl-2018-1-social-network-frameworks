2121b5f3a6b13f09de200669fc8fa9ef602e3ff6
Natalia <nataliamh.88@gmail.com>
2018-09-19 20:04:17 -0300

    ordene componentes

--- my-app/src/componentes/componentesWall/Navbar.js
+++ my-app/src/componentes/componentesWall/Navbar.js
@@ -10,43 +10,30 @@ const NavbarComponent = () => {
     return (
     <Navbar brand={<LogoNavbar photo = {ImageLogo} />} className="navbar" right>   
         <NavItem>
-
-        <Link to={routes.WALL}>
-            <Icon>home</Icon>
-            <p className="textIcon">Wall</p>
-        </Link>
+            <Link to={routes.WALL}>
+                <Icon>home</Icon>
+                <p className="textIcon">Wall</p>
+            </Link>
         </NavItem>
-
-
-           <NavItem>
-
-        <Link to={routes.PROFILE}>
-            <Icon>person</Icon>
-            <p className="textIcon">Profile</p>
-        </Link>
-        </NavItem>
-
         <NavItem>
-
-        <Link to={routes.FRIENDS}>
-
-            
-            <p className="textIcon">Friends</p>
-            <Icon>person_add</Icon>
-        </Link>
+            <Link to={routes.PROFILE}>
+                <Icon>person</Icon>
+                <p className="textIcon">Profile</p>
+            </Link>
         </NavItem>
-
-
-    <NavItem divider />
-    <NavItem>
-
-        <Link to={routes.LOGIN}>
-
-            <Icon>keyboard_tab</Icon>
-            <p className="textIcon">Log out</p>
-        </Link>    
+        <NavItem>
+            <Link to={routes.FRIENDS}>
+                <Icon>person_add</Icon>
+                <p className="textIcon">Friends</p>
+            </Link>
         </NavItem>
-
+        <NavItem divider />
+        <NavItem>
+            <Link to={routes.LOGIN}>
+                <Icon>keyboard_tab</Icon>
+                <p className="textIcon">Log out</p>
+            </Link>    
+        </NavItem>
     </Navbar>
     )
 }  
