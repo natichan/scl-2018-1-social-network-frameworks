d072f6884edb87d9f3623f95a07d2b73c01d7e74
Natalia <nataliamh.88@gmail.com>
2018-09-19 18:21:24 -0300

    rutas, cambia la ruta y lo que se muestra  pero dividido

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
-// import Login from './componentes/Login';
+import React from 'react';
+import {  BrowserRouter as Router, Route,} from 'react-router-dom';
+import Login from './componentes/Login';
 import Register from './componentes/Register';
-// import Wall from './componentes/Wall';
-// import Profile from './componentes/Profile'
+import Wall from './componentes/Wall';
+import Profile from './componentes/Profile';
+import Friends from './componentes/Friends';
+import Navigation from './componentes/Navigation'
+import * as routes from './constants/routes';
 
-class App extends Component {
-  render() {
-    return (
+const App = () =>
+  <Router>
     <div>
-        <Register />
-        {/* <Login /> */}
-        {/* <Wall /> */}
-        {/* <Profile /> */}
+      <Navigation />
+      <hr/>
+      <Route
+        exact path={routes.WALL}
+        component={Wall}
+      />
+      <Route
+        exact path={routes.REGISTER}
+        component={Register}
+      />
+      <Route
+        exact path={routes.LOGIN}
+        component={Login}
+      />
+      <Route
+        exact path={routes.PROFILE}
+        component={Profile}
+      />
+      <Route
+        exact path={routes.FRIENDS}
+        component={Friends}
+      />
     </div>
-    );
-  }
-}
+  </Router>
 
 export default App;
