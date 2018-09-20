2a9f6ecb6fcb7be2448704739bb9f82ead78d1e3
Natalia <nataliamh.88@gmail.com>
2018-09-20 20:33:55 -0300

    una forma de usar navigation en prueba

--- my-app/src/componentes/Navigation.js
+++ my-app/src/componentes/Navigation.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import {Icon, Col, Row} from 'react-materialize';
 import { Link } from 'react-router-dom';
 import * as routes from '../constants/routes';
 
 const Navigation = () => {
     return (
+      <Row>
+        <Col s={12}>
   <div>
-    <ul>
-      <li><Link to={routes.LOGIN}>Login</Link></li>
-      <li><Link to={routes.WALL}>Wall</Link></li>
-      <li><Link to={routes.PROFILE}>PROFILE</Link></li>
-      <li><Link to={routes.FRIENDS}>FRIENDS</Link></li>
-    </ul>
+     <ul s={12}>
+           <li> 
+               <Link to={routes.WALL}>
+                    <Icon>home</Icon>
+                    <p className="textIcon">Wall</p>
+                </Link>
+            </li>    
+            <li>
+                <Link to={routes.PROFILE}>
+                    <Icon>person</Icon>
+                    <p className="textIcon">Profile</p>
+                </Link>
+            </li>
+            <li>  
+                <Link to={routes.FRIENDS}>
+                    <Icon>person_add</Icon>
+                    <p className="textIcon">Friends</p>
+                </Link>
+            </li>
+            <li>  
+                <Link to={routes.LOGIN}>
+                    <Icon>keyboard_tab</Icon>
+                    <p className="textIcon">Log out</p>
+                </Link>
+            </li>    
+        </ul>
   </div>
+  </Col>
+  </Row>
   )
 }
 export default Navigation;
