9784794418cfef2707a5422712fac7cf51f7a0b9
milelym <milelymarcial@gmail.com>
2018-09-20 20:26:49 -0300

    cambios post

--- my-app/src/componentes/componentesWall/Post.js
+++ my-app/src/componentes/componentesWall/Post.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import {Row, Input,Collection, CollectionItem} from 'react-materialize'
+import { Input} from 'react-materialize'
 import firebase from "firebase"
 import './wall.css'
 class App extends Component {
