a3a31eed43246f6e30cb47dcd9ee71230addfcbe
Natalia <nataliamh.88@gmail.com>
2018-09-19 21:22:53 -0300

    navbar con rutas y ordenado

--- my-app/src/componentes/componentesWall/Navbar.js
+++ my-app/src/componentes/componentesWall/Navbar.js
@@ -9,31 +9,32 @@ import * as routes from '../../constants/routes';
 const NavbarComponent = () => {
     return (
     <Navbar brand={<LogoNavbar photo = {ImageLogo} />} className="navbar" right>   
-        <NavItem>
-            <Link to={routes.WALL}>
-                <Icon>home</Icon>
-                <p className="textIcon">Wall</p>
-            </Link>
-        </NavItem>
-        <NavItem>
-            <Link to={routes.PROFILE}>
-                <Icon>person</Icon>
-                <p className="textIcon">Profile</p>
-            </Link>
-        </NavItem>
-        <NavItem>
-            <Link to={routes.FRIENDS}>
-                <Icon>person_add</Icon>
-                <p className="textIcon">Friends</p>
-            </Link>
-        </NavItem>
-        <NavItem divider />
-        <NavItem>
-            <Link to={routes.LOGIN}>
-                <Icon>keyboard_tab</Icon>
-                <p className="textIcon">Log out</p>
-            </Link>    
-        </NavItem>
+        <ul>
+           <li> 
+               <Link to={routes.WALL}>
+                    <Icon>home</Icon>
+                    <p className="textIcon">Wall</p>
+                </Link>
+            </li>    
+            <li>
+                <Link to={routes.PROFILE}>
+                    <Icon>person</Icon>
+                    <p className="textIcon">Profile</p>
+                </Link>
+            </li>
+            <li>  
+                <Link to={routes.FRIENDS}>
+                    <Icon>person_add</Icon>
+                    <p className="textIcon">Friends</p>
+                </Link>
+            </li>
+            <li>  
+                <Link to={routes.LOGIN}>
+                    <Icon>keyboard_tab</Icon>
+                    <p className="textIcon">Log out</p>
+                </Link>
+            </li>    
+        </ul>
     </Navbar>
     )
 }  
