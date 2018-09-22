23575305b8f0554e58959e2be9adb6b4178b08b4
milelym <milelymarcial@gmail.com>
2018-09-22 01:56:47 -0300

    contenido eliminado

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -60,9 +60,6 @@ class App extends Component {
         </Col>
         <Col l={12}>
           <Input type='text'placeholder='¿Que tal tu día Veggie?'  s={12} 
-                    // autoFocus={true}
-                    // multiline={true}
-                    // rowsMax={3}
                     onChange={event => this.setState({ text: event.target.value })}
                     value={this.state.text}
                     onKeyPress={this.onSubmit}/>
