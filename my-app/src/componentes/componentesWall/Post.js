cd39a12234d5262ab4d0b979104278461516c159
milelym <milelymarcial@gmail.com>
2018-09-22 01:38:18 -0300

    eliminar contenido

--- my-app/src/componentes/componentesWall/Post.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react"
-import { Input} from 'react-materialize'
-import firebase from "firebase"
-import './wall.css'
-export default class App extends Component {
-
-
-}
