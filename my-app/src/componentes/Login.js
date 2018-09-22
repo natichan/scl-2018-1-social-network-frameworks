c80dd1709bb919f0374806fedee66bc6a3bfb9c0
Natalia <nataliamh.88@gmail.com>
2018-09-22 16:48:24 -0300

    unido registro en una vista con login

--- my-app/src/componentes/Login.js
+++ my-app/src/componentes/Login.js
@@ -3,7 +3,6 @@ import Img from './componentesLogin/Img';
 import ImagenLogo from '../assets/img/logo.png'
 import ButtonFG from './componentesLogin/ButtonFG'
 import LoginForm from './componentesLogin/LoginForm'
-import RegisterButton from './componentesLogin/RegisterButton';
 import {Row, Card, Col} from 'react-materialize';
 
 
@@ -16,7 +15,6 @@ class Login extends Component {
               <Img photo= {ImagenLogo}/>
               <LoginForm />
               <ButtonFG />
-              <RegisterButton />       
             </Card> 
           </Col>
         </Row>
