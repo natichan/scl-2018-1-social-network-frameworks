e887f860cab001e7dc7d4e2346e2910a9321def2
milelym <milelymarcial@gmail.com>
2018-09-22 02:54:02 -0300

    card

--- my-app/src/componentes/Friends.js
+++ my-app/src/componentes/Friends.js
@@ -8,7 +8,9 @@ class Friends extends Component {
       return (
         <Row  className='green'>
           <Navbar /> 
-
+          <Col className='RowFriends' s={12}>
+          <Card className=' card '></Card>
+        </Col>
         </Row> 
       );
     }
