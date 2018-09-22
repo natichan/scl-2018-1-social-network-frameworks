778ec222d22871fbca4c3add8f357b275f82a263
milelym <milelymarcial@gmail.com>
2018-09-21 21:56:53 -0300

    se elimina el Switch por no usar, se refleja error en la terminal

--- my-app/src/componentes/componentesLogin/RegisterButton.js
+++ my-app/src/componentes/componentesLogin/RegisterButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Login.css'
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Register from '../Register';
 import './Login.css';
 
