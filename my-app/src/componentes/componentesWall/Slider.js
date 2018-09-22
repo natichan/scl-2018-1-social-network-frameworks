5a748107eadec2033471eca6c191e7eb025237bb
milelym <milelymarcial@gmail.com>
2018-09-22 01:44:18 -0300

    import a usar status prueba

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -7,6 +7,13 @@ import firebase from "firebase"
 import './wall.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+	text: "", 
+	messages: [] }
+  }
+
   
   }
 
