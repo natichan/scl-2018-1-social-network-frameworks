97a48de4c8e347633cdc63ba39195704810cbd78
milelym <milelymarcial@gmail.com>
2018-09-22 01:42:09 -0300

    import

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -5,10 +5,10 @@ import imgh from './../../assets/img/veganHamburguer.jpg'
 import imgI from './../../assets/img/vegan-food-selection-t.jpg'
 import imgJ from './../../assets/img/green-kitchen-stories-meal-inspiration.jpg'
 import firebase from "firebase"
-
 import './wall.css'
-class App extends Component {
 
+class App extends Component {
+  
   }
 
 export default App;
