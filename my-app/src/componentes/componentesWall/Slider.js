7a0a8116cc5bf069b4139339977557022729b396
milelym <milelymarcial@gmail.com>
2018-09-22 01:41:11 -0300

    prueba

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
 //import ImagenLogo from './../../assets/img/veganHamburguer.jpg';
-import {Row, Slider, Slide, Col, Input, Button} from 'react-materialize'
+import {Row, Slider, Slide, Col, Input} from 'react-materialize'
 import imgh from './../../assets/img/veganHamburguer.jpg'
 import imgI from './../../assets/img/vegan-food-selection-t.jpg'
 import imgJ from './../../assets/img/green-kitchen-stories-meal-inspiration.jpg'
+import firebase from "firebase"
 
 import './wall.css'
 class App extends Component {
-    render() {
-      return (
-        <Row className=' white inputxt '>
-          <Col  s={12} className=' imgu' >
-          <Slider >
-            <Slide className='imgu' src={imgh} >
-            </Slide>
-            <Slide src={imgI}>
-            </Slide>
-            <Slide src={imgJ}>
-            </Slide>
-          </Slider> 
-          </Col>
-          <Col l={12}>
-          <Input type='text'placeholder='¿Que tal tu día Veggie?'  s={12} />
-          <Button waves='light' className='green' s={12}>Publicar</Button>
 
-          </Col>
-        </Row> 
-      );
-    }
   }
 
 export default App;
