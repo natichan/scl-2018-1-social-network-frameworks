7cf445f9e121c944587b44186ee9014d909f5aab
milelym <milelymarcial@gmail.com>
2018-09-22 01:43:04 -0300

    eliminar impor t

--- my-app/src/componentes/componentesWall/Slider.js
+++ my-app/src/componentes/componentesWall/Slider.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import ImagenLogo from './../../assets/img/veganHamburguer.jpg';
 import {Row, Slider, Slide, Col, Input} from 'react-materialize'
 import imgh from './../../assets/img/veganHamburguer.jpg'
 import imgI from './../../assets/img/vegan-food-selection-t.jpg'
