10f1f597fb6007512aa736909504af3cba863a87
milelym <milelymarcial@gmail.com>
2018-09-22 01:37:52 -0300

    usado en otro archv

--- my-app/src/componentes/componentesWall/Post.js
+++ my-app/src/componentes/componentesWall/Post.js
@@ -3,25 +3,6 @@ import { Input} from 'react-materialize'
 import firebase from "firebase"
 import './wall.css'
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { 
-	text: "", 
-	messages: [] }
-  }
 
-  componentDidMount() {
-    var config = {
-        apiKey: "AIzaSyD-O18UUFp3xJIPqSPPnLwrj-R_k4LoDAU",
-        authDomain: "redsocialreact-bf847.firebaseapp.com",
-        databaseURL: "https://redsocialreact-bf847.firebaseio.com",
-        projectId: "redsocialreact-bf847",
-        storageBucket: "redsocialreact-bf847.appspot.com",
-        messagingSenderId: "335447925232"
-    }
-
-    firebase.initializeApp(config)
-    this.getMessages()
-  }
 
 }
