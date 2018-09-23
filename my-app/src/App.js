0db2d7d036a3952724ac5fe307f333102892bf63
Natalia <nataliamh.88@gmail.com>
2018-09-23 01:28:34 -0300

    probando perfil

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import Wall from './componentes/Wall';
-// import Auth from './componentes/Auth'
-
+//import Wall from './componentes/Wall';
+import Auth from './componentes/Auth'
+import Profile from './componentes/Profile'
 class App extends Component {
   render() {
     return (
      <div>
        {/* <Auth /> */}
-       <Wall/>
+       {/* <Wall/> */}
+       <Profile />
      </div>
     )
   };
