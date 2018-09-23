3881793fc0823466531f25812bb8db2dfdeae204
milelym <milelymarcial@gmail.com>
2018-09-23 19:00:35 -0300

    rutas listas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 // import Wall from './componentes/Wall';
-import Auth from './componentes/Auth'
+//import Auth from './componentes/Auth'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Profile from "./componentes/Profile";
+import  Friends from "./componentes/Friends";
+import  Auth from "./componentes/Auth";
+import  Wall from "./componentes/Wall";
 
 class App extends Component {
   render() {
     return (
-     <div>
-       <Auth />
-       {/* <Wall/> */}
-     </div>
+    //  <div>
+    //    <Auth />
+    //    {/* <Wall/> */}
+    //  </div>
+    <Router basename={process.env.PUBLIC_URL}>
+    <div>
+      <Route exact path="/" component={Auth} />
+      <Route path="/Wall" component={Wall} />     
+      <Route path="/Profile" component={Profile} />     
+      <Route path="/Friends" component={Friends} /> 
+    </div>          
+  </Router>
     )
   };
 };
