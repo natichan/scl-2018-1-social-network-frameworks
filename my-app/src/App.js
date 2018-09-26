72162b71e63270cf97988cd2fd6a61dc7a27c17a
Natalia <nataliamh.88@gmail.com>
2018-09-26 11:20:39 -0300

    borre comentarios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import Wall from './componentes/Wall';
-//import Auth from './componentes/Auth'
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Profile from "./componentes/Profile";
 import  Friends from "./componentes/Friends";
@@ -10,10 +8,6 @@ import  Wall from "./componentes/Wall";
 class App extends Component {
   render() {
     return (
-    //  <div>
-    //    <Auth />
-    //    {/* <Wall/> */}
-    //  </div>
     <Router basename={process.env.PUBLIC_URL + '/'}>
     <div>
       <Route exact path="/" component={Auth} />
