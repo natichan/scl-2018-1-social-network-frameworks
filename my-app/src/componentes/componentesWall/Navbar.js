14d810643cf26fa0d5e1da06d13499fd2207aa1c
Natalia <nataliamh.88@gmail.com>
2018-09-26 08:53:26 -0300

    rutas

--- my-app/src/componentes/componentesWall/Navbar.js
+++ my-app/src/componentes/componentesWall/Navbar.js
@@ -15,7 +15,7 @@ class NavbarComponent extends Component {
      logout() {
         fire.auth().signOut();
         console.log('click');
-        window.location = '/';
+        window.location = {process.env.PUBLIC_URL + '/'};
     }
     
     render() {
@@ -23,7 +23,7 @@ class NavbarComponent extends Component {
             <Navbar brand={<LogoNavbar photo = {ImageLogo} />} className="navbar" right>   
                 <NavItem href={process.env.PUBLIC_URL + '/Wall'}><span className="textIcon">Wall</span><Icon>home</Icon></NavItem>
                 <NavItem href={process.env.PUBLIC_URL + '/Profile'}><span className="textIcon">Profile</span><Icon>person</Icon></NavItem>
-                <NavItem href='/Friends'><span className="textIcon">Friends</span><Icon>person_add</Icon></NavItem>
+                <NavItem href={process.env.PUBLIC_URL + '/Friends'}><span className="textIcon">Friends</span><Icon>person_add</Icon></NavItem>
                 <NavItem onClick={this.logout} ><span className="textIcon">Log out</span><Icon>keyboard_tab</Icon></NavItem>       
             </Navbar>
         )
