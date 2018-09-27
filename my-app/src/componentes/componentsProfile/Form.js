f603f138d76580eef0ed2f5fc9d269f97b29939f
Natalia <nataliamh.88@gmail.com>
2018-09-27 14:23:35 -0300

    se muestra imagen y nombre de persona

--- my-app/src/componentes/componentsProfile/Form.js
+++ my-app/src/componentes/componentsProfile/Form.js
@@ -1,19 +1,44 @@
 import React, {Component} from 'react';
 import './profile.css';
-import {Row, Input, Button} from 'react-materialize';
+import {Container, Row, Col} from 'react-materialize';
+import fire from '../../config/firebase';
+import deff from './../../assets/img/default.png';
+import './profile.css';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        name: '',
+        photo: '',
+    };
+}
+
+componentDidMount() {
+    return fire.auth().onAuthStateChanged((user) => {
+      // console.log(user); 
+        this.setState({
+            name: user.displayName,
+            photo: user.photoURL,
+        });
+    });          
+}
     render() {
         return (
-            <Row>           
-                <Input s={6} label="First Name" />
-                <Input s={6} label="Last Name" />
-                <Input label="Birthday" s={12} />
-                <Input type="textarea" label="About me" s={12}/> 
-                <div className="center">
-                <Button s={3} waves='light' node='a' href='' className="button"> Save </Button>
-                </div>
+          <div>
+          <header>
+            <Row className="center">
+              <Col s={12}>
+              <img className="picture" src={this.state.photo === ""? deff: this.state.photo} alt="user" value={this.state.photo}></img>
+              </Col>
             </Row>
+            </header>
+            <Row className="center">
+              <Col s={12}>
+              <h1>{this.state.name}</h1>
+              </Col>
+            </Row>
+          </div>
         )
     }
 }
