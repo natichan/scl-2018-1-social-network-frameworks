d80287bc39db3f0085967a6b45a13868d8d3ca7a
Natalia <nataliamh.88@gmail.com>
2018-09-27 14:49:21 -0300

    nombre y forma listo

--- my-app/src/componentes/componentsProfile/Form.js
+++ my-app/src/componentes/componentsProfile/Form.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './profile.css';
-import {Container, Row, Col} from 'react-materialize';
+import {Row, Col} from 'react-materialize';
 import fire from '../../config/firebase';
 import deff from './../../assets/img/default.png';
 import './profile.css';
@@ -25,19 +25,21 @@ componentDidMount() {
 }
     render() {
         return (
-          <div>
-          <header>
-            <Row className="center">
-              <Col s={12}>
-              <img className="picture" src={this.state.photo === ""? deff: this.state.photo} alt="user" value={this.state.photo}></img>
-              </Col>
-            </Row>
+          <div className="back">
+            <header>
+              <Row className="center">
+                <Col s={12}>
+                <img className="picture" src={this.state.photo === ""? deff: this.state.photo} alt="user" value={this.state.photo}></img>
+                </Col>
+              </Row>
             </header>
-            <Row className="center">
-              <Col s={12}>
-              <h1>{this.state.name}</h1>
-              </Col>
-            </Row>
+            <main>
+              <Row className="center">
+                <Col s={12}>
+                <h1>{this.state.name}</h1>
+                </Col>
+              </Row>
+            </main>
           </div>
         )
     }
